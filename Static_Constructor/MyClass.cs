32d28ce41583550db06b07a02319ad184e41ce0e
Gowwre <56794916+Gowwre@users.noreply.github.com>
2022-09-20 16:09:22 +0700

    Create MyClass.cs

---
+++ Static_Constructor/MyClass.cs
@@ -0,0 +1,18 @@
+﻿namespace Static_Constructor;
+
+public class MyClass
+{
+    public static int x = 1;
+
+    static MyClass()
+    {
+        x = 2;
+        Console.WriteLine("Static constructor: x={0}",x);
+    }
+
+    public MyClass()
+    {
+        x++;
+        Console.WriteLine("Object constructor: x={0}",x);
+    }
+}
