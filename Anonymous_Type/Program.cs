ea0dd14fb8773d947071d12782c3ef6f164b9421
Gowwre <56794916+Gowwre@users.noreply.github.com>
2022-09-20 17:56:25 +0700

    Update Program.cs

--- Anonymous_Type/Program.cs
+++ Anonymous_Type/Program.cs
@@ -4,4 +4,4 @@ var obj1 = new { id = 1000,name = "Vee" };
 Console.WriteLine($"id:{obj1.id},name={obj1.name}");
 
 dynamic obj2 = new { id = 2000, name = "Scott", Email = "scott@gmail.com" };
-Console.WriteLine($"id ");
+Console.WriteLine($"id:{obj2.id}, name:{obj2.name}, email:{obj2.Email} ");
