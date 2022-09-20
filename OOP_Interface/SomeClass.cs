e391409046bbf6c319732868b586b8f2dba28553
Gowwre <56794916+Gowwre@users.noreply.github.com>
2022-09-20 14:53:15 +0700

    Create SomeClass.cs

---
+++ OOP_Interface/SomeClass.cs
@@ -0,0 +1,15 @@
+﻿namespace OOP_Interface;
+
+public class SomeClass:ISomeInterface
+{
+    public string SomeProperty { get; set; }
+    public string SomeMethod()
+    {
+        return "Lol";
+    }
+
+    public void SomethingElse()
+    {
+        Console.WriteLine(DateTime.Now);
+    }
+}
