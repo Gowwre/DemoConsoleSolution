b36cf80cc4eda4b613441df3be1ba79bec96a8ff
Gowwre <56794916+Gowwre@users.noreply.github.com>
2022-09-20 17:32:04 +0700

    Update Program.cs

---
+++ Anonymous_Type/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var obj1 = new { id = 1000,name = "Vee" };
+Console.WriteLine($"id:{obj1.id},name={obj1.name}");
+
+dynamic obj2 = new { id = 2000, name = "Scott", Email = "scott@gmail.com" };
+Console.WriteLine($"id ");
