127103a9576b3da303ec42af365feae72219556b
Gowwre <56794916+Gowwre@users.noreply.github.com>
2022-09-20 20:35:30 +0700

    Added SamplePoint.cs

---
+++ readonly_const_Keyword/SamplePoint.cs
@@ -0,0 +1,23 @@
+﻿namespace readonly_const_Keyword;
+
+public class SamplePoint
+{
+    public int x;
+
+    public readonly int y = 26;
+    public readonly int z;
+
+    public const int t = 100;
+
+    public SamplePoint()
+    {
+        z = 24;
+    }
+
+    public SamplePoint(int p1, int p2, int p3, int p4 = 50)
+    {
+        x = p1;
+        y = p2;
+        z = p3;
+    }
+}
