e8a9161ba3117c24980ebbc7bd802c6775b51598
Gowwre <56794916+Gowwre@users.noreply.github.com>
2022-09-20 15:10:59 +0700

    Create MyClass.cs

---
+++ OOP_Interface_Inheritance/MyClass.cs
@@ -0,0 +1,19 @@
+﻿namespace OOP_Interface_Inheritance;
+
+public class MyClass:IJamesBondCar
+{
+    public void Drive()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Dive()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void TurboBoost()
+    {
+        throw new NotImplementedException();
+    }
+}
