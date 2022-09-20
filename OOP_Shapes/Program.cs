ad788fde78bbbac796dcf9543673860d058158ce
Gowwre <56794916+Gowwre@users.noreply.github.com>
2022-09-20 14:16:03 +0700

    Added main program

--- OOP_Shapes/Program.cs
+++ OOP_Shapes/Program.cs
@@ -1,3 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using OOP_Shapes;
+
+Shape circle = new Circle();
+Rectangle rectangle = new Rectangle();
+circle.Display();
+circle.Draw();
+rectangle.Draw();
+rectangle.Display();
+rectangle.Print();
