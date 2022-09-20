e9f83b08b59d25c43decbc7f80c3d862eb8d3805
Gowwre <56794916+Gowwre@users.noreply.github.com>
2022-09-20 15:30:08 +0700

    Update Program.cs

--- is_as_Operators/Program.cs
+++ is_as_Operators/Program.cs
@@ -1,3 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using is_as_Operators;
+
+Rectangle rectangle = new Rectangle(1.2f, 21.5f);
+ICalculate calculate;
+if (rectangle is ICalculate)
+{
+    calculate = rectangle as ICalculate;
+    Console.WriteLine("Area: {0:F2}", calculate.Area());
+}
+else
+{
+    Console.WriteLine("Interface method not implemented");
+}
