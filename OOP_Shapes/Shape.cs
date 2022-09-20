2544273c6b3c41c991e1ed21775318573f3ce793
Gowwre <56794916+Gowwre@users.noreply.github.com>
2022-09-20 11:40:45 +0700

    Added Shape implementation

---
+++ OOP_Shapes/Shape.cs
@@ -0,0 +1,22 @@
+﻿namespace OOP_Shapes;
+
+public abstract class Shape
+{
+    public int Height { get; set; }
+    public int Width { get; set; }
+    
+    //Virtual Method
+    public virtual void Draw()
+    {
+        Console.WriteLine("Performing base class Drawing tasks");
+        
+    }
+
+    public void Print()
+    {
+        Console.WriteLine("Performing base class Printing tasks");
+    }
+
+    public abstract void Display();
+    
+}
