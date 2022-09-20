37ec0c1c61d4a209906f66d0969dbd9f43b03083
Gowwre <56794916+Gowwre@users.noreply.github.com>
2022-09-20 14:09:42 +0700

    Added Rectangle.cs

---
+++ OOP_Shapes/Rectangle.cs
@@ -0,0 +1,19 @@
+﻿namespace OOP_Shapes;
+
+public class Rectangle:Shape
+{
+    public override void Draw()
+    {
+        Console.WriteLine("Drawing a rectangle");
+    }
+
+    public override void Display()
+    {
+        Console.WriteLine("Display Rectangle");
+    }
+
+    public new void Print()
+    {
+        Console.WriteLine("Print Rectangle");
+    }
+}
