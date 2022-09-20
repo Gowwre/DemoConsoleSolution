6f8f24ddbccab7c57827f3ac978abad3fe57ab0e
Gowwre <56794916+Gowwre@users.noreply.github.com>
2022-09-20 16:34:47 +0700

    Update Program.cs

--- Static_Constructor/Program.cs
+++ Static_Constructor/Program.cs
@@ -1,3 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using Static_Constructor;
+
+MyClass m1 = new MyClass();
+MyClass.x=4;
+MyClass m2 = new MyClass();
