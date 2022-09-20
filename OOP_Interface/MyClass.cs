3d2894c3c24ddb2a2926652e62a92f4d1e58c8b7
Gowwre <56794916+Gowwre@users.noreply.github.com>
2022-09-20 15:00:13 +0700

    Added main program code

---
+++ OOP_Interface/MyClass.cs
@@ -0,0 +1,19 @@
+﻿namespace OOP_Interface;
+
+public class MyClass:IFirst,ISecond
+{
+    void IFirst.Print()
+    {
+        Console.WriteLine("IFirst's print method");
+    }
+
+    public void Display()
+    {
+        Console.WriteLine("Display method");
+    }
+
+    void ISecond.Print()
+    {
+        Console.WriteLine("ISecond's print method");
+    }
+}
--- OOP_Interface/Program.cs
+++ OOP_Interface/Program.cs
@@ -1,3 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using OOP_Interface;
+
+MyClass obj = new MyClass();
+obj.Display();
+IFirst first = obj;
+first.Print();
+ISecond second = obj;
+second.Print();
