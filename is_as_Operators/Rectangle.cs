28258daf2cd8de5d35f94ae6adcf854fd4adbcf2
Gowwre <56794916+Gowwre@users.noreply.github.com>
2022-09-20 15:26:35 +0700

    Create Rectangle.cs

---
+++ is_as_Operators/Rectangle.cs
@@ -0,0 +1,18 @@
+﻿namespace is_as_Operators;
+
+public class Rectangle:ICalculate
+{
+    private float _length;
+    private float _width;
+
+    public Rectangle(float length, float width)
+    {
+        _length = length;
+        _width = width;
+    }
+
+    public double Area()
+    {
+        return _length * _width;
+    }
+}
