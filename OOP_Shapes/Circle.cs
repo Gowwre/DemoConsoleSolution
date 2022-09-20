541be7c8a8e172b464afade72f1afcbba02ee639
Gowwre <56794916+Gowwre@users.noreply.github.com>
2022-09-20 11:42:44 +0700

    Create Circle.cs

---
+++ OOP_Shapes/Circle.cs
@@ -0,0 +1,14 @@
+﻿namespace OOP_Shapes;
+
+public class Circle:Shape
+{
+    public override void Draw()
+    {
+        Console.WriteLine("Draw a circle");
+    }
+
+    public override void Display()
+    {
+        Console.WriteLine("Display Circle");
+    }
+}
