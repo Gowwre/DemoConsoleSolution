cbb01792a9103d93e1ab64f59e01b442bd9632cc
Gowwre <56794916+Gowwre@users.noreply.github.com>
2022-09-20 17:00:34 +0700

    Create Location.cs

---
+++ Expression-Bodied_Members/Location.cs
@@ -0,0 +1,14 @@
+﻿namespace Expression_Bodied_Members;
+
+public class Location
+{
+    private string locationName;
+
+    public Location(string name) => Name = name;
+
+    public string Name
+    {
+        get => locationName;
+        set => locationName = value;
+    }
+}
