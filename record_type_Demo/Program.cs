4e2b2d4092affcae0b63e4f3d95cc4c5f8262cb6
Gowwre <56794916+Gowwre@users.noreply.github.com>
2022-09-21 07:56:06 +0700

    Update Program.cs

--- record_type_Demo/Program.cs
+++ record_type_Demo/Program.cs
@@ -1,3 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using record_type_Demo;
+
+Customer customer01 = new Customer { Name = "Vee", Age = 22 };
+customer01.Print();
+Customer customer02 = customer01 with { Name = "John" };
+customer02.Print();
+Customer customer03 = new();
+customer03.Print();
