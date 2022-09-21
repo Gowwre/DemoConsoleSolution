19ab7f32c7cb9eb648e296de5c48741b44807893
Gowwre <56794916+Gowwre@users.noreply.github.com>
2022-09-21 08:05:18 +0700

    Updated Program.cs

--- Declaration_Demo/Program.cs
+++ Declaration_Demo/Program.cs
@@ -1,3 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+try
+{
+    using var reader = new System.IO.StreamReader(@"D:\vee.txt");
+    var content = reader.ReadToEnd();
+    Console.WriteLine($"File length:{content.Length}");
+}
+catch (System.IO.FileNotFoundException fileNotFoundException)
+{
+    Console.WriteLine(fileNotFoundException.Message);
+}
+catch (Exception exception)
+{
+    Console.WriteLine(exception.Message);
+}
