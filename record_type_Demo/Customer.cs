55ac461c9646362797d794fb8be6fab4d0a8afb4
Gowwre <56794916+Gowwre@users.noreply.github.com>
2022-09-21 07:53:35 +0700

    Added Customer.cs

---
+++ record_type_Demo/Customer.cs
@@ -0,0 +1,12 @@
+﻿namespace record_type_Demo;
+
+public record Customer()
+{
+    public string Name { get; init; } = "New customer";
+    public int Age { get; init; } = 20;
+
+    public void Print()
+    {
+        Console.WriteLine($"Name:{Name}, Age:{Age}");
+    }
+}
